aed4e8c50ec9cef8beb44449990f768b0242f85a
konamadithya <135402175+konamadithya@users.noreply.github.com>
2025-06-26 21:37:31 +0530

    Add files via upload

---
+++ task3.sql
@@ -0,0 +1,16 @@
+-- Task 3: Basic SELECT Queries
+
+-- Example: Select all columns from a table
+SELECT * FROM students;
+
+-- Example: Select specific columns
+SELECT name, age FROM students;
+
+-- Example: Using WHERE clause
+SELECT * FROM students WHERE age > 18;
+
+-- Example: Using ORDER BY
+SELECT * FROM students ORDER BY age DESC;
+
+-- Example: Using LIMIT
+SELECT * FROM students LIMIT 5;
